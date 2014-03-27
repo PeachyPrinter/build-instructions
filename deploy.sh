717dbddf108fdc0632c2b5628e239abe46c82fc6
Michael Nash <mnash@jglobal.com>
2014-03-27 12:57:42 -0600

    Use verbose option for logging

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 remote=go@instructions.peachyprinter.com:/var/www/instructions/
  for i in `find . -mtime -1 -type f -print | sed '/\.git/d'`
  do
-   scp $i  ${remote}$(echo $i | sed 's:^\.*/::')
+   scp -v $i ${remote}$(echo $i | sed 's:^\.*/::')
  done
 
