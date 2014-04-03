e33360599e53d7734122f3a1913df2375692c9c3
Michael Nash <mnash@jglobal.com>
2014-04-03 10:25:21 -0600

    Fix image paths, modify deploy script to deploy optimized version after build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 remote=go@instructions.peachyprinter.com:/var/www/instructions/
- for i in `find . -mtime -1 -type f -print | sed '/\.git/d'`
- do
-   echo "Copying $i"
-   scp $i ${remote}$(echo $i | sed 's:^\.*/::')
- done
+scp -r dist/* ${remote}
+# for i in `find . -mtime -1 -type f -print | sed '/\.git/d'`
+# do
+#   echo "Copying $i"
+#   scp $i ${remote}$(echo $i | sed 's:^\.*/::')
+# done
 
