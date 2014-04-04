53b185f986a518b198bc399500006205bd761231
Michael Nash <mnash@jglobal.com>
2014-04-04 11:35:06 -0600

    Use rsync to speed up deploy's

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,3 @@
 remote=go@instructions.peachyprinter.com:/var/www/instructions/
-scp -r dist/* ${remote}
-# for i in `find . -mtime -1 -type f -print | sed '/\.git/d'`
-# do
-#   echo "Copying $i"
-#   scp $i ${remote}$(echo $i | sed 's:^\.*/::')
-# done
+rsync -vr dist/* ${remote}
 
